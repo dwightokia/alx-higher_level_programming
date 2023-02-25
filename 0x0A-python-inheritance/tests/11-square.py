04341c81171a5fef4306dbf3549c97efe4287760
Okia Dwight <gabrielokia5@gmail.com>
2023-02-25 13:49:12 -0800

    done

--- 0x0A-python-inheritance/tests/11-square.py
+++ 0x0A-python-inheritance/tests/11-square.py
@@ -1,25 +1,16 @@
 #!/usr/bin/python3
-"""
-Contains definition for the class Square
-"""
-
+"""Defines a Rectangle subclass Square."""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Definition of class square that inherits from class Rectangle"""
+    """Represent a square."""
 
     def __init__(self, size):
-        """Initializes instance of the class Square"""
-        super().integer_validator("size", size)
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
-
-    def area(self):
-        """Method for computing Square area"""
-        return self.__size ** 2
-
-    def __str__(self):
-        """Returns string representation of an instance of class square"""
-        return "[{}] {}/{}".format(type(self).__name__, self.__size,
-                                   self.__size)
